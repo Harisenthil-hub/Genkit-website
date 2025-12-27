cd676ea7493414808b0498e4be175bec6460064d
Harisenthil-hub <hari367854@gmail.com>
2025-12-27 20:43:45 +0530

    vercel test

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/Genkit-website">  {/*Should remove base on deployment */}
+    <BrowserRouter>  {/*Should remove base on deployment basename="/Genkit-website"*/}
       <App />
     </BrowserRouter>
 )
