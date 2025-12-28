598c0f8012eb067fabc43e502171dd3497ad1cd6
Harisenthil-hub <hari367854@gmail.com>
2025-12-28 22:59:40 +0530

    deploy host

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Genkit-website/", // should be removed in production
+  base: "/Genkit-website/", // should be removed in production
 })
